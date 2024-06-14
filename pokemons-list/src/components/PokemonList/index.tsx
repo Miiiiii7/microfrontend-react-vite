79fb477f7cfbba578af8337abf83bb06d12a1a4b
Mirian Ito <mnio@gft.com>
2024-06-14 08:33:40 -0300

    chore: change 3 pokemon list

--- pokemons-list/src/components/PokemonList/index.tsx
+++ pokemons-list/src/components/PokemonList/index.tsx
@@ -3,12 +3,13 @@ import { DataGrid, GridColDef, GridRowsProp } from "@mui/x-data-grid";
 
 const PokemonList = () => {
   const rows: GridRowsProp = [
-    { id: 1, name: "Hello", type: "World" },
-    { id: 2, name: "DataGridPro", type: "is Awesome" },
-    { id: 3, name: "MUI", type: "is Amazing" },
+    { id: 1, name: "Bulbasaur", type: "Plant" },
+    { id: 2, name: "Ivysaur", type: "Plant" },
+    { id: 3, name: "Venusaur", type: "Plant" },
   ];
 
   const columns: GridColDef[] = [
+    { field: "id", headerName: "Nº", width: 150 },
     { field: "name", headerName: "Name", width: 150 },
     { field: "type", headerName: "Type", width: 150 },
   ];
